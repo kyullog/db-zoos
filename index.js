3871e767db3db2bab2f95c8fe532c8ab369e51dd
Jonathan Dorety <kyullog@gmail.com>
2019-03-15 11:32:16 -0400

    Added get all endpoint

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const { development } = require('./data/knexfile')
+const db = knex(development);
 
 const server = express();
 
@@ -7,6 +11,17 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const getZoos = await db.select().from('zoos');
+    console.log(getZoos);
+    res.status(200).json(getZoos);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({error: "There was problem processing your request",
+  err})
+  }
+})
 
 const port = 3300;
 server.listen(port, function() {
