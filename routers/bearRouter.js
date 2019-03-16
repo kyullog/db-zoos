a9a38d5bb9b6a2b8132478317e5bdc50792a6827
Jonathan Dorety <kyullog@gmail.com>
2019-03-16 12:58:00 -0400

    implement get all bears endpoint

--- routers/bearRouter.js
+++ routers/bearRouter.js
@@ -4,4 +4,14 @@ const bearRouter = express.Router();
 
 const db = require('../data/dbConfig');
 
+bearRouter.get('/', async (req, res) => {
+  try {
+    const bears = await db.select().from('bears');
+    console.log(bears);
+    res.status(200).json(bears);
+  } catch {
+    res.status(500).json({err: "There was a problem processing your request"})
+  }
+})
+
 module.exports = bearRouter;
