85e7275fa6acdb7f3898d7f0110fe8b522bc32fa
Jonathan Dorety <kyullog@gmail.com>
2019-03-15 20:42:11 -0400

    Create delete endpoint

--- index.js
+++ index.js
@@ -49,6 +49,21 @@ server.get('/api/zoos/:id', async (req, res) => {
   }
 })
 
+server.delete('/api/zoos/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deleted = await db('zoos').where({id: id}).del();
+    console.log(deleted);
+    if (deleted) {
+    res.status(200).json({message: "Record deleted"})
+    } else {
+      res.status(400).json({error: "No record with that id to be deleted"})
+    }
+  } catch {
+    res.status(500).json({error: "There was a problem processing your request"})
+  }
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
